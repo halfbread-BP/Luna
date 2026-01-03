2890b87c94e89b83e6665e7da6055e141e5e1ba2
halfbread-BP <oznerol.discord.gg@gmail.com>
2026-01-04 00:21:27 +0100

    Create GameObject.lua

---
+++ Scripts/GameObject.lua
@@ -0,0 +1,32 @@
+GameObject = {}
+GameObject.__index = GameObject
+
+function GameObject:new(name)
+  local gameObject = {
+    name = name or "GameObject",
+    components = {},
+    transform = nil, -- holds transform component
+    active = true,
+  }
+  setmetatable(gameObject, GameObject)
+
+  -- Assign a GameObject the Transform component
+  gameObject:addComponent(transform)
+
+  return gameObject
+end
+
+function GameObject:addComponent(componentClass)
+  local component = componentClass:new(self)
+  table.insert(self.components, component)
+  return component
+end
+
+function GameObject:getComponent(componentClass)
+  for _, component in ipairs(self.components) do
+    if getmetatable(component) == componentClass then
+      return component
+    end
+  end
+  return nil
+end
